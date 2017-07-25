b8773b954cedf7b1a45d7cf7831c59bfc8b3a904
VB91 <vburroughs21@gmail.com>
2017-07-25 16:09:32 -0500

    Assignment: Layout

---
+++ layout/src/app/main-component/main-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main-component',
+  templateUrl: './main-component.component.html',
+  styleUrls: ['../app.component.css', './main-component.component.css']
+})
+export class MainComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
